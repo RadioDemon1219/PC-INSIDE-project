b49d31c7bbf255d04695c304a5eabc058271083f
Nalivaika Aliaxandr <nalivaikasasha@gmail.com>
2025-05-05 18:25:35 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+            if (targetSection) {
+                window.scrollTo({
+                    top: targetSection.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+
+    fetch('./xml/content.xml')
+        .then(response => {
+            return response.text();
+        })
+        .then(data => {
+            const parser = new DOMParser();
+            const xmlDoc = parser.parseFromString(data, 'text/xml');
+            const paragraphElement = xmlDoc.querySelector('paragraph');
+            const paragraphContent = paragraphElement.textContent.trim();
+            console.log('Содержимое абзаца:', paragraphContent.substring(0, 50) + '...');
+            const introParagraphDiv = document.getElementById('intro-paragraph');
+            if (introParagraphDiv) {
+                introParagraphDiv.appendChild(paragraph);
+            } else {
+            }
+        })
+});
